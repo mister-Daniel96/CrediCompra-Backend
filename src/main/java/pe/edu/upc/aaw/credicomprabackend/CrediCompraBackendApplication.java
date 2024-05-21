908a631e7632c1fd67b5a5e3d39b00d3e7dfb037
Juan Ayala <juandanielayalaquinones@gmail.com>
2024-05-21 11:27:54 -0500

    ProyectoInicio

---
+++ src/main/java/pe/edu/upc/aaw/credicomprabackend/CrediCompraBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.credicomprabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediCompraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrediCompraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/aaw/credicomprabackend/CrediCompraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.credicomprabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediCompraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
